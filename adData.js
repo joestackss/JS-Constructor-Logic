2564ac64d8785cec65c91863541998d18cee8e25
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-08 12:44:01 +0100

    html, css, Js

---
+++ adData.js
@@ -0,0 +1,22 @@
+const adData = {
+  facebook: {
+    site: "Facebook",
+    adViews: 23477,
+    clicks: 210,
+    conversions: 5,
+  },
+  twitter: {
+    site: "Twitter",
+    adViews: 13904,
+    clicks: 192,
+    conversions: 9,
+  },
+  instagram: {
+    site: "Instagram",
+    adViews: 3256,
+    clicks: 200,
+    conversions: 2,
+  },
+};
+
+export default adData;
